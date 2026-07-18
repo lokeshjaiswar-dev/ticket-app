af0fc418888e34b51b789237c06a8b9ed28b0dca
Lokesh Jaiswar <lokeshjaiswar.dev@gmail.com>
2026-07-18 18:28:07 +0530

    push

--- backend/config/db.js
+++ backend/config/db.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 const connectDB = async () => {
   try {
     const conn = await mongoose.connect(process.env.MONGO_URI);
-    console.log(`MongoDB Connected: ${conn.connection.host} asdfasd`);
+    console.log(`MongoDB Connected: ${conn.connection.host} asdfa`);
   } catch (error) {
     console.error(`MongoDB Connection Error: ${error.message}`);
     process.exit(1);
