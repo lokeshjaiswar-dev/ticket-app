68127c6c237bba9a8049dcd4c6055504d4e455c1
Lokesh Jaiswar <lokeshjaiswar.dev@gmail.com>
2026-07-21 10:13:20 +0530

    crm code update

--- backend/config/db.js
+++ backend/config/db.js
@@ -5,7 +5,7 @@ const connectDB = async () => {
     const conn = await mongoose.connect(process.env.MONGO_URI);
     console.log(`MongoDB Connected: ${conn.connection.host}`);
   } catch (error) {
-    console.error(`MongoDB Connection Error: ${error.message} my name is lokesh jai`);
+    console.error(`MongoDB Connection Error: ${error.message} my name is lokesh j`);
     process.exit(1);
   }
 };
