dac66ca811540b940d26faaf3617e2d484d65a5c
Lokesh Jaiswar <lokeshjaiswar.dev@gmail.com>
2026-07-27 18:16:03 +0530

    push

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -8,7 +8,7 @@ exports.login = async (req, res) => {
 
     let user = await User.findOne({ email });
     if (!user && email === 'admin@datastraw.com' && password === 'admin123') {
-      user = new User({ name: 'Admin User lokesh jais', email, password });
+      user = new User({ name: 'Admin User lokesh ja', email, password });
       await user.save();
     }
 
