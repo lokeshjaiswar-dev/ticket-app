fb2b058b418f6e91bb5e78f09b7ae95d7129ed38
Lokesh Jaiswar <lokeshjaiswar.dev@gmail.com>
2026-07-31 19:55:47 +0530

    push

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -21,8 +21,6 @@ exports.login = async (req, res) => {
       return res.status(401).json({ message: 'Invalid Credentials' });
     }
 
-    console.log("")
-
     const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET, { expiresIn: '1d' });
     res.json({ token, user: { id: user._id, name: user.name, email: user.email } });
   } catch (err) {
